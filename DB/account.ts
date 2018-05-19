eb39b03f2f65ef798a2dd9c0204418fb9f9321c5
cotsell <cotsell@gmail.com>
2018-05-20 02:40:52 +0900

    유저프로필 이미지 업로드 수정. 이미지 변경 시 기존 이미지 삭제 기능 필요.

--- DB/account.ts
+++ DB/account.ts
@@ -48,137 +48,140 @@ export default class Account {
   // DB에 해당 유저가 존재하는지 찾아보고, 없으면 삽입해요.
   public async insertNewUser(user: UserInfo): Promise<Result> {
 
-      // ID와 Password에 이상이 있을 때의 필터링.
-      if (user.id === undefined || user.id === null || user.id === '' ||
-          user.password === undefined || user.password === null || user.password === ''
-      ) {
-          return new Result(false, 'ID혹은 패스워드가 비었거나, 문제가 있어요.');
-      }
+    // ID와 Password에 이상이 있을 때의 필터링.
+    if (user.id === undefined || user.id === null || 
+        user.id === '' ||
+        user.password === undefined || user.password === null || 
+        user.password === '') {
+      return new Result(false, 'ID혹은 패스워드가 비었거나, 문제가 있어요.');
+    }
 
-      // 결과가 없으면 0, 있으면 1.
-      let result: any = await this.model.count({ id: user.id });
+    // 결과가 없으면 0, 있으면 1.
+    let result: any = await this.model.count({ id: user.id });
 
-      if (result >= 1) {
+    if (result >= 1) {
 
-          console.log('이미 있는 사용자에요.\n');
-          return new Result(false, '이미 있는 사용자.');
+      console.log('이미 있는 사용자에요.\n');
+      return new Result(false, '이미 있는 사용자.');
 
+    } else {
+
+      const userPassword = user.password || '';
+      const encryptedPassword = crypto.createHmac('sha1', conf.SECRET)
+      .update(userPassword)
+      .digest('base64');
+
+      let doc: Document = new this.model({
+        id: user.id,
+        password: encryptedPassword,
+        grade: 9,
+        nickName: user.nickName,
+        profileImgUrl: '',
+        totalThumbUp: 0,
+        signature: '',
+      });
+
+      result = await doc.save().catch(err => { return null; });
+      
+      if (result === null) {
+        return new Result(false, 'ID혹은 닉네임이 중복되었어요.');
       } else {
-
-          const userPassword = user.password || '';
-          const encryptedPassword = crypto.createHmac('sha1', conf.SECRET)
-              .update(userPassword)
-              .digest('base64');
-
-          let doc: Document = new this.model({
-              id: user.id,
-              password: encryptedPassword,
-              grade: 9,
-              nickName: user.nickName,
-              profileImgUrl: '',
-              // myScrapIdList: [],
-              totalThumbUp: 0,
-              signature: '',
-          });
-
-          result = await doc.save().catch(err => { return null; });
-          
-          if (result === null) {
-              return new Result(false, 'ID혹은 닉네임이 중복되었어요.');
-          } else {
-              return new Result(true, '가입 성공', 0, result);
-          }
+        return new Result(true, '가입 성공', 0, result);
       }
+    }
   }
 
-    // 해당 조건의 사용자를 찾아서 있으면 1 없으면 0을 리턴해요.
-    public async checkIdAndPassword(user: UserInfo): Promise<number> {
-        const encryptedPassword = crypto.createHmac('sha1', conf.SECRET)
-        .update(user.password + '')
-        .digest('base64');
+  // 해당 조건의 사용자를 찾아서 있으면 1 없으면 0을 리턴해요.
+  public async checkIdAndPassword(user: UserInfo): Promise<number> {
+    const encryptedPassword = crypto.createHmac('sha1', conf.SECRET)
+    .update(user.password + '')
+    .digest('base64');
 
-        console.log(encryptedPassword);
-        
-        let result = await this.model.count({ id: user.id, password: encryptedPassword });
-        return result;
-    }
+    console.log(encryptedPassword);
+    
+    let result = await this.model.count(
+      { id: user.id, password: encryptedPassword });
 
-    // 유저 한명의 정보를 찾아서 돌려줘요.
-    public async findUserOne(user: UserInfo): Promise<Document | null> {
-        let result = await this.model.findOne({ id: user.id }, { _id: false, password: false });
-        return result;
-    }
+    return result;
+  }
 
-    // 두명 이상의 유저 정보를 돌려줍니다. 사실 한명도 되긴 해요. 
-    public async findUsers(ids: string[]) {
-        let conditions: Object[] = [];
-        let condition = {};
-        ids.map(value => {
-            conditions.push({ id: value });
-        });
-        condition = Object.assign({}, { $or: [...conditions] });
+  // 유저 한명의 정보를 찾아서 돌려줘요.
+  public async findUserOne(user: UserInfo): Promise<Document | null> {
+    let result = await this.model.findOne(
+      { id: user.id }, { _id: false, password: false });
 
-        const result = await this.model.find(condition, { _id: false, password: false });
-        if (result.length < 1)
-            return new Result(false, '검색 결과가 없어요.');
+    return result;
+  }
 
-        return new Result(true, '검색 성공', 0, result);
-    }
+  // 두명 이상의 유저 정보를 돌려줍니다. 사실 한명도 되긴 해요. 
+  public async findUsers(ids: string[]) {
+    let conditions: Object[] = [];
+    let condition = {};
 
-    public async changePassword(userId: string, oldPass: string, newPass: string) {
-        const encryptedOldPass = crypto.createHmac('sha1', conf.SECRET)
-        .update(oldPass).digest('base64');
-        const encryptedNewPass = crypto.createHmac('sha1', conf.SECRET)
-        .update(newPass).digest('base64');
+    ids.map(value => {
+        conditions.push({ id: value });
+    });
+    condition = Object.assign({}, { $or: [...conditions] });
 
-        const result = await this.model.updateOne(
-            { id: userId, password: encryptedOldPass }, 
-            { $set: { password: encryptedNewPass, updatedTime: Date.now() } }
-        ).catch(err => {
-            console.log(err);
-            return null;
-        });
+    const result = await this.model.find(condition, 
+      { _id: false, password: false });
 
-        if (result === null)
-            return new Result(false, '변경 실패.');
-        if (JSON.parse(JSON.stringify(result))['nModified'] === 0)
-            return new Result(false, '변경 실패.');
+    if (result.length < 1)
+      return new Result(false, '검색 결과가 없어요.');
 
-        return new Result(true, '변경 완료.', 0, result);
-    }
+    return new Result(true, '검색 성공', 0, result);
+  }
 
-    public async updateUserInfo(userId: string, userInfo: UserInfo){
-        const updateResult = await this.model.updateOne(
-            { id: userId }, 
-            { $set: userInfo })
-        .catch(err => { 
-            console.error(err);
-            return null;
-            }
-        );
+  public async changePassword(userId: string, oldPass: string, newPass: string) {
+    const encryptedOldPass = crypto.createHmac('sha1', conf.SECRET)
+    .update(oldPass)
+    .digest('base64');
 
-        if (Util.parse(updateResult)['nModified'] === 1)
-            return new Result(true, '변경 완료', 0, updateResult);
-        
-        return new Result(false, '변경 실패.');
+    const encryptedNewPass = crypto.createHmac('sha1', conf.SECRET)
+    .update(newPass)
+    .digest('base64');
 
-    }
+    const result: any = await this.model.updateOne(
+      { id: userId, password: encryptedOldPass }, 
+      { $set: { password: encryptedNewPass, updatedTime: Date.now() } })
+      .catch(err => { console.log(err); return null; });
 
-    // 유저 이미지를 변경해요.
-    public async updateUserImgUrl(userId: string, imgPath: string) {
-      const result: any = await this.model.findOneAndUpdate(
-        { id: userId },
-        { $set: { profileImgUrl: imgPath, updatedTime: Date.now() } },
-        { new: false })
-      .catch(err => { console.error(err); return null; });
+    if (result === null)
+      return new Result(false, '변경 실패.');
 
-      if (result === null)
-        return new Result(false, '변경 하는 도중에 문제가 발생했어요.');
-      
-      console.log(result);
-      const oldUrl = result.profileImgUrl;
+    if (result.nModified === 0)
+      return new Result(false, '변경 실패.');
 
-      return new Result(true, '변경 완료.', 0, 
-        { oldUrl: oldUrl, newUrl: imgPath });
-    }
+    return new Result(true, '변경 완료.', 0, result);
+  }
+
+  public async updateUserInfo(userId: string, userInfo: UserInfo){
+    const updateResult: any = await this.model.updateOne(
+      { id: userId }, 
+      { $set: userInfo })
+    .catch(err => { console.error(err); return null; });
+
+    if (updateResult.nModified === 1)
+      return new Result(true, '변경 완료', 0, updateResult);
+    
+    return new Result(false, '변경 실패.');
+  }
+
+  // 유저 이미지를 변경해요.
+  public async updateUserImgUrl(userId: string, imgPath: string) {
+    const result: any = await this.model.findOneAndUpdate(
+      { id: userId },
+      { $set: { profileImgUrl: imgPath, updatedTime: Date.now() } },
+      { new: false })
+    .catch(err => { console.error(err); return null; });
+
+    if (result === null)
+      return new Result(false, '변경 하는 도중에 문제가 발생했어요.');
+    
+    console.log(result);
+    const oldUrl = result.profileImgUrl;
+
+    return new Result(true, '변경 완료.', 0, 
+      { oldUrl: oldUrl, newUrl: imgPath });
+  }
 }
--- routes/account.ts
+++ routes/account.ts
@@ -15,7 +15,7 @@ const storage = Multer.diskStorage({
 		cb(null, 'userProfileImgs');
 	},
 	filename: (req, file, cb) => {
-        cb(null, file.originalname + '-' + Date.now());
+    cb(null, file.originalname + '-' + Date.now());
 	}
 });
 const multer = Multer({ storage: storage });
@@ -25,160 +25,163 @@ const multer = Multer({ storage: storage });
 
 // AccessToken으로 로그인 처리.
 route.get('/check', (req, res) => {
-    const accessToken = req.get('c-access-token');
-    
-    // string형태만 받을 수 있어서, 강제로 빈 공백을 더해줘서 string으로 캐스팅 했어요.
-    let result = jwt.verify(accessToken);
-    res.json(new Result(result));
+  const accessToken = req.get('c-access-token');
+  
+  let result = jwt.verify(accessToken);
+  res.json(new Result(result));
 });
 
 // 가입처리.
 route.post('/signup', (req, res) => {
-    console.log(`${ req.body.id } 라는 id로 가입신청을 시도합니다.\n`);
+  console.log(`${ req.body.id } 라는 id로 가입신청을 시도합니다.\n`);
 
-    Mongo.getAccount().insertNewUser(req.body)
-        .then(value => {
-            // console.log(value);
-            let result = value;
+  Mongo.getAccount().insertNewUser(req.body)
+  .then(value => {
+    let result = value;
 
-            if (value.result === true) {
+    if (value.result === true) {
 
-                let accessToken = jwt.createJwt({ userId: value.payload.id });
+      let accessToken = jwt.createJwt({ userId: value.payload.id });
 
-                result = Object.assign({}, value,
-                    { payload: Object.assign({}, value.payload, { accessToken: accessToken })}
-                );
-            }
+      // result = Object.assign({}, value,
+      //   { payload: Object.assign({}, value.payload, { accessToken: accessToken })}
+      // );
 
-            console.log('가입 신청 응답 결과:');
-            console.log(JSON.stringify(result) + '\n');
-            res.json(result);
-        });
+      result = Object.assign({}, value,
+        { payload: { ...value.payload, accessToken: accessToken } });
+
+    }
+
+    console.log('가입 신청 응답 결과:');
+    console.log(JSON.stringify(result) + '\n');
+    res.json(result);
+  });
 });
 
 // 로그인
 route.post('/login', (req, res) => {
-    let { id, password } = req.body;
-    console.log(`${ id } 유저가 로그인을 시도합니다.\n`);
+  let { id, password } = req.body;
+  console.log(`${ id } 유저가 로그인을 시도합니다.\n`);
 
-    Mongo.getAccount().checkIdAndPassword(req.body)
-        .then(value => {
-            if (value === 1) {
-                let accessToken = jwt.createJwt({ userId: id });
-                res.json(new Result(true, '로그인 완료.', 0, { accessToken: accessToken }));
-            } else {
-                res.json(new Result(false, 'ID혹은 비밀번호가 틀렸어요.'));
-            }
-        })
+  Mongo.getAccount().checkIdAndPassword(req.body)
+  .then(value => {
+    if (value === 1) {
+      let accessToken = jwt.createJwt({ userId: id });
+      res.json(new Result(true, '로그인 완료.', 0, { accessToken: accessToken }));
+    } else {
+      res.json(new Result(false, 'ID혹은 비밀번호가 틀렸어요.'));
+    }
+  })
 });
 
 route.post('/user', (req, res) => {
-    const { id } = req.body; 
-    console.log(`${ id } 유저의 정보 조회 요청이 들어왔어요.\n`);
+  const { id } = req.body; 
+  console.log(`${ id } 유저의 정보 조회 요청이 들어왔어요.\n`);
 
-    Mongo.getAccount().findUserOne(req.body)
-        .then(value => {
-            let result: any;
+  Mongo.getAccount().findUserOne(req.body)
+  .then(value => {
+    let result: any;
 
-            if (value === null) {
-                console.log(value);
-                result = { result: false, msg: '해당 유저의 정보가 없어요.\n' };
-            } else {
-                console.log(value);
-                result = { result: true, payload: value };
-            }
-            res.json(result);
-        });
+    if (value === null) {
+      console.log(value);
+      result = { result: false, msg: '해당 유저의 정보가 없어요.\n' };
+    } else {
+      console.log(value);
+      result = { result: true, payload: value };
+    }
+    res.json(result);
+  });
 });
 
 // 두명 이상의 유저 정보를 돌려줍니다. 사실 한명도 되긴 해요. 
 route.post('/users', (req, res) => {
-    const ids = req.body['ids'];
-    Mongo.getAccount().findUsers(ids)
-    .then(value => {
-        res.json(value);
-    });
+  const ids = req.body['ids'];
+
+  Mongo.getAccount().findUsers(ids)
+  .then(value => {
+    res.json(value);
+  });
 });
 
 // 회원의 비밀번호를 변경합니다. 
 route.post('/user/changePassword', (req, res) => {
-    const accessToken = req.headers['c-access-token'] + '';
-    const oldPass = req.body['oldPass'] + '';
-    const newPass = req.body['newPass'] + '';
+  const accessToken = req.get('c-access-token');
+  const oldPass = req.body['oldPass'] + '';
+  const newPass = req.body['newPass'] + '';
 
-    if (!jwt.verify(accessToken))
-        return new Result(false, '엑세스토큰에 이상이 있어요.', ACCESS_TOKEN_ERROR);
+  if (!jwt.verify(accessToken))
+    res.json(new Result(false, '엑세스토큰에 이상이 있어요.', ACCESS_TOKEN_ERROR));
 
-    if (oldPass === undefined || oldPass === null || oldPass === '' ||
-        newPass === undefined || newPass === null || newPass === '')
-        return new Result(false, '잘못된 유형의 비밀번호에요.');
+  if (oldPass === undefined || oldPass === null || oldPass === '' ||
+      newPass === undefined || newPass === null || newPass === '')
+    res.json(new Result(false, '잘못된 유형의 비밀번호에요.'));
 
-    const userId = JSON.parse(JSON.stringify(jwt.decode(accessToken)))['userId'];
+  const userId = jwt.decode(accessToken)['userId'];
 
-    console.log(`${userId} 유저의 패스워드 변경 요청이 들어왔어요.`);
+  console.log(`${userId} 유저의 패스워드 변경 요청이 들어왔어요.`);
 
-    Mongo.getAccount().changePassword(userId, oldPass, newPass)
-    .then(value => {
-        console.log(`${userId} 유저의 패스워드 변경 요청 결과에요.`);
-        console.log(value.msg);
-        console.log(value.payload);
-        
-        res.json(value);
-    });
+  Mongo.getAccount().changePassword(userId, oldPass, newPass)
+  .then(value => {
+    console.log(`${userId} 유저의 패스워드 변경 요청 결과에요.`);
+    console.log(value.msg);
+    console.log(value.payload);
+    
+    res.json(value);
+  });
 });
 
 // 회원의 비밀번호가 맞는지 확인해줍니다.
 route.post('/user/checkPassword', (req, res) => {
-    const accessToken = req.headers['c-access-token'] + '';
-    const pass = req.body['password'] + '';
+  const accessToken = req.headers['c-access-token'] + '';
+  const pass = req.body['password'] + '';
 
-    if (!jwt.verify(accessToken))
-        return new Result(false, '엑세스토큰에 이상이 있어요.', ACCESS_TOKEN_ERROR);
+  if (!jwt.verify(accessToken))
+    res.json(new Result(false, '엑세스토큰에 이상이 있어요.', ACCESS_TOKEN_ERROR));
 
-    if (pass === undefined || pass === null || pass === '')
-        return new Result(false, '잘못된 유형의 비밀번호에요.');
+  if (pass === undefined || pass === null || pass === '')
+    res.json(new Result(false, '잘못된 유형의 비밀번호에요.'));
 
-    const userId = JSON.parse(JSON.stringify(jwt.decode(accessToken)))['userId'];
+  const userId = JSON.parse(JSON.stringify(jwt.decode(accessToken)))['userId'];
 
-    console.log(`${userId} 유저의 패스워드 확인 요청이 들어왔어요.`);
+  console.log(`${userId} 유저의 패스워드 확인 요청이 들어왔어요.`);
 
-    Mongo.getAccount().checkIdAndPassword({ id: userId, password: pass })
-    .then(value => {
-        if (value === 1)
-            res.json(new Result(true, '확인 완료.'));
-        else 
-            res.json(new Result(false, '틀린 비밀번호에요.'));
-    });
+  Mongo.getAccount().checkIdAndPassword({ id: userId, password: pass })
+  .then(value => {
+    if (value === 1)
+      res.json(new Result(true, '확인 완료.'));
+    else 
+      res.json(new Result(false, '틀린 비밀번호에요.'));
+  });
 });
 
 // 회원 정보 변경해줘요.
 route.post('/user/update', (req, res) => {
-    const accessToken = req.get('c-access-token');
-    const userInfo: UserInfo = req.body;
+  const accessToken = req.get('c-access-token');
+  const userInfo: UserInfo = req.body;
 
-    if (!jwt.verify(accessToken))
-        return new Result(false, '엑세스토큰에 이상이 있어요.', ACCESS_TOKEN_ERROR);
+  if (!jwt.verify(accessToken))
+    res.json(new Result(false, '엑세스토큰에 이상이 있어요.', ACCESS_TOKEN_ERROR));
 
-    const userId = jwt.decode(accessToken)['userId'];
-    
-    if (hasNickNameError(userInfo.nickName)) 
-        return new Result(false, '닉네임 유형이 잘못되었어요.');
+  const userId = jwt.decode(accessToken)['userId'];
+  
+  if (hasNickNameError(userInfo.nickName)) 
+    res.json(new Result(false, '닉네임 유형이 잘못되었어요.'));
 
-    Mongo.getAccount().updateUserInfo(userId, userInfo)
-        .then(result => {
-            res.json(result);
-        });
+  Mongo.getAccount().updateUserInfo(userId, userInfo)
+  .then(result => {
+    res.json(result);
+  });
 
-    // ---- 정리용도 함수 모음 ----
+  // ---- 정리용도 함수 모음 ----
 
-    // 닉네임 에러 체크함수에요. 문제가 있으면 true를 리턴해요.
-    // 따로 만든 이유는 에러 체크할 사항이 늘어나게 되면 복잡하니까..
-    function hasNickNameError(nickName: string | undefined): boolean {
-        if (userInfo.nickName === undefined || userInfo.nickName === '')
-            return true;
-        else
-            return false;
-    }
+  // 닉네임 에러 체크함수에요. 문제가 있으면 true를 리턴해요.
+  // 따로 만든 이유는 에러 체크할 사항이 늘어나게 되면 복잡하니까..
+  function hasNickNameError(nickName: string | undefined): boolean {
+    if (userInfo.nickName === undefined || userInfo.nickName === '')
+      return true;
+    else
+      return false;
+  }
 });
 
 route.post('/user/changeUserImg', multer.single('userimg'), (req, res) => {
